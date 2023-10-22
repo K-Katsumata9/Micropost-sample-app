0f6ed858284b01e7d1949f46b78aea5dbb9a7e61
K-Katsumata9 <dowbler@icloud.com>
2023-10-22 17:48:38 +0000

    ユーザーの更新・表示・削除をできるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_user,     only: [:destroy]
+
+	def index
+		#@users = User.all
+		@users = User.paginate(page: params[:page])
+	end
+
   def show
 	# => app/views/users/show_html.erb
 		@user = User.find(params[:id])
@@ -21,9 +30,60 @@ class UsersController < ApplicationController
 			render  "new"
 		end
   end
-	def user_params
-		params.require(:user).permit(
-			:name, :email, :password, 
-			:password_confirmation)
-	end
+
+	def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う。
+			flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+	def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to "/users"
+  end
+
+
+
+
+
+
+
+  #private以下のメソッドは書き換え不可となる
+	private
+		def user_params
+			params.require(:user).permit(
+				:name, :email, :password, 
+				:password_confirmation)
+		end
+		
+		# ログイン済みユーザーかどうか確認
+		def logged_in_user
+			unless logged_in? #if notでも可
+				store_location
+				flash[:danger] = "Please log in."
+				redirect_to login_url
+			end
+		end
+
+		# 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url)  if not @user == current_user #unlessでも可
+    end
+
+		# 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+		
 end 
