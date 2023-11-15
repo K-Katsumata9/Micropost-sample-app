e300d47e4e3aa9765fc3e50fc198207d628236d2
K-Katsumata9 <dowbler@icloud.com>
2023-11-15 03:18:49 +0000

    フォロー機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.create!(name:  "Koki Katsumata",
+# ユーザー
+User.create!(name:  "K Katsumata",
              email: "sample@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
@@ -18,6 +19,7 @@ User.create!(name:  "Koki Katsumata",
                activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
 	content = Faker::Lorem.sentence(word_count: 5)
@@ -25,3 +27,11 @@ users = User.order(:created_at).take(6)
 		user.microposts.create!(content: content)
 	end
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
